8e5cf7f998dba9bef3743f29f6c6d18536fec53b
romanshalimov <120426185+romanshalimov@users.noreply.github.com>
2023-01-12 15:15:13 +0300

    2.2-databases-2

---
+++ 2.2-databases-2/m2m-relations/articles/admin.py
@@ -0,0 +1,32 @@
+from django.contrib import admin
+
+from .models import Article, Scope, Tag
+from django.forms import BaseInlineFormSet
+from django.core.exceptions import ValidationError
+
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    pass
+
+class ScopeInlineFormset(BaseInlineFormSet):
+    def clean(self):
+        count_is_main = 0
+        for form in self.forms:
+            is_main = form.cleaned_data.get('is_main')
+            if is_main:
+                count_is_main += 1
+
+            if count_is_main > 1:
+                raise ValidationError('Поле is_main должно быть указано только 1 раз')
+        return super().clean()  # вызываем базовый код переопределяемого метода
+
+
+class ScopeInline(admin.TabularInline):
+    model = Scope
+    extra = 6
+    formset = ScopeInlineFormset
+
+
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    inlines = [ScopeInline]
